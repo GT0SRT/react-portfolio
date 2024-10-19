77f573cee11015b939d5a6005a9c364d5450ed6a
Gourav Makode <140704003+GT0SRT@users.noreply.github.com>
2024-10-19 14:46:03 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I&apos;m melon</h1>
+        <p className={styles.description}>
+          I&apos;m a B-Tech CSE Student at UIT. Reach out if you&apos;d like to learn more!
+        </p>
+        <a href="mailto: melon-react@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
